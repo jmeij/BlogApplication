f34395b8c945e7841d1501f9def602df8fcb6ac4
Jeroen Meij <jeroen.meij@madaster.com>
2024-05-27 21:49:09 +0200

    Added topbar

--- Blog.SPA/src/app/app.module.ts
+++ Blog.SPA/src/app/app.module.ts
@@ -8,35 +8,45 @@ import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/c
 import { FormsModule, ReactiveFormsModule, } from '@angular/forms'
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AdminComponent } from './components/admin/admin.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserService } from './services/user.service';
+import { TopbarComponent } from './components/shared/topbar/topbar.component';
 
 
-@NgModule({ declarations: [
-        AppComponent,
-        AdminComponent,
-        OverviewComponent,
-        LoginComponent
+@NgModule({
+    declarations: [
+      AppComponent,
+      AdminComponent,
+      OverviewComponent,
+      LoginComponent,
+      TopbarComponent
     ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatSelectModule], providers: [
-        provideClientHydration(),
-        provideAnimationsAsync(),
-        provideHttpClient(withFetch()),
-        UserService,
-        provideHttpClient(withInterceptorsFromDi())
+    bootstrap: [AppComponent],
+    imports: [BrowserModule,
+      AppRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MatButtonModule,
+      MatDividerModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatInputModule,
+      MatSelectModule,
+      MatToolbarModule
+    ],
+    providers: [
+      provideClientHydration(),
+      provideAnimationsAsync(),
+      provideHttpClient(withFetch()),
+      UserService,
+      provideHttpClient(withInterceptorsFromDi())
     ] })
 export class AppModule { }
